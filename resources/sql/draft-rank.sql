742676a89aa03ec999a7f27c2afdc4a4f5635dbf
Dennis Lipovsky <dennis.lipovsky@gmail.com>
2016-08-21 10:07:22 -0400

    tons of queries to get data during draft

--- resources/sql/draft-rank.sql
+++ resources/sql/draft-rank.sql
@@ -15,4 +15,73 @@ where espn_data.player_id = :id;
 -- name: find-player-by-id
 select id, name, position, team, bye
 from player
-where id = :id
+where id = :id;
+
+-- name: find-player-by-name
+select id, name, position, team, bye, d.espn_rank
+from player
+join espn_data d on p.id = d.player_id
+where name = :name;
+
+-- name: create-fantasy-team<!
+insert into fantasy_team (name, owner)
+values (:name, :owner);
+
+-- name: save-draft-pick<!
+insert into draft_pick (player_id, owner_id, round)
+values (:player_id, :owner_id, :round);
+
+-- name: save-draft-keeper<!
+insert into draft_pick (player_id, owner_id, round, keeper)
+values (:player_id, :owner_id, :round, true);
+
+-- name: all-teams
+select id, name, owner from fantasy_team;
+
+-- name: find-players-like
+select id, name, position, team, bye, d.rank as espn_rank
+from player p
+join espn_data d on p.id = d.player_id
+where name like :query
+
+-- name: draft-results
+select t.name as owner, p.name, position, team, bye, round
+from draft_pick dp
+join player p on dp.player_id = p.id
+join fantasy_team t on t.id = dp.owner_id
+order by round, t.name;
+
+-- name: undrafted
+select name, position, team, bye, d.rank as espn_rank
+from player p
+join espn_data d on p.id = d.player_id
+where not exists (select 1 from draft_pick dp where p.id = dp.player_id)
+order by espn_rank
+limit :limit;
+
+-- name: undrafted-by-position
+select name, position, team, bye, d.rank as espn_rank
+from player p
+join espn_data d on p.id = d.player_id
+where not exists (select 1 from draft_pick dp where p.id = dp.player_id)
+and position = :position
+order by espn_rank
+limit :limit;
+
+-- name: undrafted-by-position-not-bye-week
+select name, position, team, bye, d.rank as espn_rank
+from player p
+join espn_data d on p.id = d.player_id
+where not exists (select 1 from draft_pick dp where p.id = dp.player_id)
+and position = :position
+and bye not in (:bye)
+order by espn_rank
+limit :limit
+
+-- name: team-roster
+select name, position, team, bye, d.rank as espn_rank
+from player p
+join espn_data d on p.id = d.player_id
+join draft_pick dp on dp.player_id = p.id
+where dp.owner_id = :owner_id
+order by dp.round
--- src/undrafted/core.clj
+++ src/undrafted/core.clj
@@ -5,6 +5,8 @@
 
 (def espn-300 "http://www.espn.com/fantasy/football/story/_/id/16287927/2016-fantasy-football-rankings-fantasy-football-player-rankings-top-fantasy-football-players-fantasy-football-draft")
 
+(def espn-injury "http://games.espn.com/ffl/resources/playernews?&injuryMode=true")
+
 (def db-spec {:classname "org.postgresql.Driver"
               :subprotocol "postgresql"
               :subname "//localhost:5432/undrafted"
@@ -33,16 +35,72 @@
         players (map-indexed parse-espn-player (map #(html/select % [:td]) rankings))]
     players))
 
+(defn scrape-espn-injury []
+  (let [{:keys [status body error]} @(http/get espn-injury {:as :stream})
+        html (html/html-resource body)
+        ]
+    html))
+
 (defn save-espn-player [player]
   (let [{:keys [id]} (create-player<! player)
         player (assoc player :id id)]
     (insert-espn-data! player)
     player))
 
+(defn teams []
+  (let [format-fn (fn [{:keys [id name owner]}] (format "%-10s | %-25s | %-20s" id name owner))]
+    (println (format-fn {:id "id" :name "name" :owner "owner"}))
+    (println (apply str (take 61 (repeat "-"))))
+    (println (clojure.string/join "\n" (map format-fn (all-teams))))))
 
+(defn player [query]
+  (find-players-like {:query (str "%" query "%")}))
 
+(defn result-formatter [{:keys [name position team bye espn_rank]}]
+  (format "%-25s | %-4s | %-5s | %-2s | %-4s" name position team bye espn_rank))
+
+(defn pprint-result [result]
+  (println (result-formatter {:name "name" :position "pos" :team "team" :bye "bye" :espn_rank "rank"}))
+  (println (apply str (take 54 (repeat "-"))))
+  (println (clojure.string/join "\n" (map result-formatter result)))
+  result)
 
 (comment
   (def espn-full (scrape-espn))
   (save-espn-player (first espn-full))
+  (def html-resource (scrape-espn-injury))
+  (def table-resource  (html/select html-resource [:table :table :tr]))
+
+
+  (teams)
+
+  (def keepers ["Benjamin Watson" "Ronnie Hillman" "Andre Ellington" "Brandon Marshall" "Mike Wallace" "Colin Kaepernick" "Zach Ertz" "Steven Hauschka" "Mark Ingram"])
+  (pprint-result (map (comp first player) keepers))
+  
+  (pprint-result (undrafted {:limit 10}))
+
+  (pprint-result (undrafted-by-position {:position "QB" :limit 10}))
+  (pprint-result (undrafted-by-position {:position "RB" :limit 10}))
+  (pprint-result (undrafted-by-position {:position "WR" :limit 10}))
+  (pprint-result (undrafted-by-position {:position "TE" :limit 10}))
+  (pprint-result (undrafted-by-position {:position "K" :limit 10}))
+  (pprint-result (undrafted-by-position {:position "DST" :limit 10}))
+
+
+  (pprint-result (undrafted-by-position-not-bye-week
+                  {:position "QB" :limit 10 :bye [11]}))
+  (pprint-result (undrafted-by-position-not-bye-week
+                  {:position "RB" :limit 10 :bye [11]}))
+  (pprint-result (undrafted-by-position-not-bye-week
+                  {:position "WR" :limit 10 :bye [11]}))
+  (pprint-result (undrafted-by-position-not-bye-week
+                  {:position "TE" :limit 10 :bye [11]}))
+  (pprint-result (undrafted-by-position-not-bye-week
+                  {:position "K" :limit 10 :bye [11]}))
+  (pprint-result (undrafted-by-position-not-bye-week
+                  {:position "DST" :limit 10 :bye [11]}))
+
+
+  (pprint-result (team-roster {:owner_id 6}))
+
   )
